9cf162541d34ae441f2e733c8ea429f0cab0c4f4
Dmitry <dmi3z@yandex.ru>
2020-02-20 20:27:49 +0300

    update one

--- main.js
+++ main.js
@@ -124,10 +124,8 @@ app.post('/products', (req, res) => {
 
 app.post('/product', (req, res) => {
     const product = req.body;
-    const id = product.id;
-    console.log(id);
-    delete product.id;
-    console.log(product);
+    const id = product._id;
+    delete product._id;
     productsdb.collection('products').updateOne({_id: ObjectID(id)}, {$set: {...product } }, (err) => {
         if (err) {
             return res.sendStatus(500);
